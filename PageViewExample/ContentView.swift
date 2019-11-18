07fecda8db6b8d63173a2f5944ae1c8210c2d9f1
Rodrigo Paixão <rodrigo.paixao@foton.la>
2019-11-18 09:48:30 -0300

    Initial Commit

---
+++ PageViewExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PageViewExample
+//
+//  Created by FOTON on 18/11/19.
+//  Copyright © 2019 Rodrigo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
